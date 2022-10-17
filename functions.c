80f8021d843235eb5b31be8b2de0513af3bc9e65
CorneliusU91 <110995458+CorneliusU91@users.noreply.github.com>
2022-10-18 00:18:42 +0100

    Update functions.c

--- functions.c
+++ functions.c
@@ -122,6 +122,7 @@ int print_int(va_list types, char buffer[],
 	long int n = va_arg(types, long int);
 	unsigned long int num;
 	n = convert_size_number(n, size);
+	
 	if (n == 0)
 		buffer[i--] = '0';
 	buffer[BUFF_SIZE - 1] = '\0';
@@ -137,7 +138,7 @@ int print_int(va_list types, char buffer[],
 		num /= 10;
 	}
 	i++;
-	return (write_number(is_negative, i, buffer, flags, width, precision,size));
+	return (write_number(is_negative, i, buffer, flags, width, precision, size));
 }
 
 /************************* PRINT BINARY *************************/
@@ -175,6 +176,7 @@ int print_binary(va_list types, char buffer[],
 	for (i = 0, sum = 0, count = 0; i < 32; i++)
 	{
 		sum += a[i];
+		
 		if (sum || i == 31)
 		{
 			char z = '0' + a[i];
