09116fa49b7eb6a7eda683a35f72f113e90adf4b
CorneliusU91 <110995458+CorneliusU91@users.noreply.github.com>
2022-10-18 00:13:32 +0100

    Update functions.c

--- functions.c
+++ functions.c
@@ -17,7 +17,6 @@ int print_char(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
 	char c = va_arg(types, int);
-	
 	return (handle_write_char(c, buffer, flags, width, precision, size));
 }
 
@@ -49,7 +48,7 @@ int print_string(va_list types, char buffer[],
 	{
 		str = "(null)";
 		if (precision >= 6)
-			str = " ";	
+			str = "";	
 	}
 	while (str[length] != '\0')
 		length++;
@@ -122,8 +121,8 @@ int print_int(va_list types, char buffer[],
 	int is_negative = 0;
 	long int n = va_arg(types, long int);
 	unsigned long int num;
-	n = convert_size_number(n, size);
 	
+	n = convert_size_number(n, size);
 	if (n == 0)
 		buffer[i--] = '0';
 	buffer[BUFF_SIZE - 1] = '\0';
@@ -162,7 +161,6 @@ int print_binary(va_list types, char buffer[],
 	unsigned int n, m, i, sum;
 	unsigned int a[32];
 	int count;
-	
 	UNUSED(buffer);
 	UNUSED(flags);
 	UNUSED(width);
@@ -179,7 +177,6 @@ int print_binary(va_list types, char buffer[],
 	for (i = 0, sum = 0, count = 0; i < 32; i++)
 	{
 		sum += a[i];
-		
 		if (sum || i == 31)
 		{
 			char z = '0' + a[i];
